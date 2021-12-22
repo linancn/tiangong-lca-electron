6f33945eb7af2cd59c79bbec4d152543d853ccaf
nanli <linanenv@gmail.com>
2021-12-22 22:51:40 +0800

    fix update button

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { app, BrowserWindow, nativeTheme, ipcMain, shell } from "electron";
 import * as path from "path";
-import { updateHandle } from "./updater/updater";
+import { AppImageUpdater, MacUpdater, NsisUpdater } from "electron-updater";
+import log from "electron-log";
+import { AllPublishOptions } from "builder-util-runtime";
 // import MenuBuilder from "./menu/menu";
-
-updateHandle();
 
 function createWindow() {
   // Create the browser window.
@@ -69,6 +69,60 @@ function createWindow() {
   });
   // load the index.html of the app.
   // mainWindow.loadFile(path.join(__dirname, "../index.html"));
+
+  const options = {
+    provider: "github",
+    owner: "linancn",
+    repo: "Electron-CrystaLCA",
+  } as AllPublishOptions;
+
+  let autoUpdater: any;
+  if (process.platform === "win32") {
+    autoUpdater = new NsisUpdater(options);
+  } else if (process.platform === "darwin") {
+    autoUpdater = new MacUpdater(options);
+  } else {
+    autoUpdater = new AppImageUpdater(options);
+  }
+
+  autoUpdater.autoDownload = false;
+
+  log.transports.file.level = "debug";
+  autoUpdater.logger = log;
+  log.info("client version", app.getVersion());
+
+  autoUpdater.on("error", (error: { stack: any } | null) => {
+    log.info(
+      "Error: ",
+      error == null ? "unknown" : (error.stack || error).toString()
+    );
+  });
+
+  autoUpdater.on("checking-for-update", () => {
+    log.info("Checking for update");
+  });
+
+  autoUpdater.on("update-available", (info: any) => {
+    log.info("Got a new client version, will auto download it", info);
+    autoUpdater.downloadUpdate();
+  });
+
+  autoUpdater.on("update-not-available", (info: any) => {
+    log.info("Current version is up-to-date", info);
+  });
+
+  autoUpdater.on("update-downloaded", (info: any) => {
+    log.info(info);
+    mainWindow.webContents.send("software-update");
+  });
+
+  autoUpdater.on("download-progress", function (progressObj: any) {
+    log.debug("download progress", progressObj);
+  });
+
+  autoUpdater.checkForUpdates();
+
+  ipcMain.on("software-upgrade", () => autoUpdater.quitAndInstall(true, true));
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
