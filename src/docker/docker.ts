2a55b959308e0a8175543a8380deb1738d46a18f
hyy1987 <779003812@qq.com>
2022-05-14 00:30:55 +0800

    fix: send data

--- src/docker/docker.ts
+++ src/docker/docker.ts
@@ -1,9 +1,13 @@
 import * as child from "child_process";
-import { BrowserWindow } from "electron";
+import { BrowserWindow, ipcMain } from "electron";
 
 export default function DockerBuider(platform: string): string {
   const mainwindow = BrowserWindow.fromId(1);
   let msg = "";
+  let closeCode: number | null = -1;
+
+  let errs: string[] = [];
+
   switch (platform) {
     case "win32":
       const dbpull = child.spawn("cmd.exe", [
@@ -12,15 +16,17 @@ export default function DockerBuider(platform: string): string {
       ]);
       dbpull.stdout.on("data", (data) => {
         console.log(`${data}`);
-        mainwindow?.webContents.send('data', `${data}`);
+        mainwindow?.webContents.send("data", `${data}`);
       });
       dbpull.stderr.on("data", (data) => {
         console.error(`${data}`);
-        mainwindow?.webContents.send('data', `${data}`);
+        errs.push(data);
+        mainwindow?.webContents.send("data", `${data}`);
       });
       dbpull.on("close", function (code) {
         console.log(`child process exited with code ${code}`);
-        mainwindow?.webContents.send('close', `${code}`);
+        closeCode = code;
+        mainwindow?.webContents.send("close", `${code}`);
       });
       break;
     case "darwin":
@@ -29,5 +35,17 @@ export default function DockerBuider(platform: string): string {
     default:
       break;
   }
+
+  ipcMain.on("code", () => {
+    mainwindow?.webContents.send("close", `${closeCode}`);
+  });
+
+  ipcMain.on("err", () => {
+    errs.forEach((err) => {
+      mainwindow?.webContents.send("data", `${err}`);
+    });
+    errs = [];
+  });
+
   return msg;
 }
