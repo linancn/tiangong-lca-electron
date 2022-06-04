b70e99be28d12e9b4f82fba78505a5e4ea95d180
nanli <linanenv@gmail.com>
2022-06-04 22:16:12 +0800

    fix docker stop

--- src/main.ts
+++ src/main.ts
@@ -150,17 +150,16 @@ app.on("ready", () => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on("window-all-closed", () => {
-  DockerStop(process.platform);
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
-
-// app.on("quit", function () {
-//   //stop running containers
-//   DockerStop(process.platform);
+// app.on("window-all-closed", () => {
+//   if (process.platform !== "darwin") {
+//     app.quit();
+//   }
 // });
+
+app.on("before-quit", function () {
+  //stop running containers
+  DockerStop(process.platform);
+});
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
